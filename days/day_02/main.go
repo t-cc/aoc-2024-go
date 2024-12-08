5152c66446833700cd8c6b446615b409d3827b9f
Toño <tonio.castro@gmail.com>
2024-12-08 19:47:21 +0100

    feat: day 02 (a)

--- days/day_02/main.go
+++ days/day_02/main.go
@@ -2,6 +2,8 @@ package day_02
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 // Run function of the daily challenge
@@ -16,10 +18,117 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	safeCount := 0
+
+	for _, lineStr := range input {
+		var line = []int{}
+		for _, char := range strings.Split(lineStr, " ") {
+			number, err := strconv.Atoi(char)
+			if err != nil {
+				panic(err)
+			}
+			line = append(line, number)
+		}
+
+		isDecreasing := line[0] > line[1]
+
+		if isDecreasing {
+			isSafe := true
+			for index, number := range line {
+				if index > 0 {
+					diff := number - line[index-1]
+					if diff > 0 {
+						// different directions
+						isSafe = false
+						break
+					}
+					if diff < -3 || diff == 0 {
+						isSafe = false
+						break
+					}
+				}
+			}
+			if isSafe {
+				safeCount++
+			}
+		} else {
+			isSafe := true
+			for index, number := range line {
+				if index > 0 {
+					diff := number - line[index-1]
+					if diff < 0 {
+						// different directions
+						isSafe = false
+						break
+					}
+					if diff > 3 || diff == 0 {
+						isSafe = false
+						break
+					}
+				}
+			}
+			if isSafe {
+				safeCount++
+			}
+		}
+	}
+
+	return strconv.Itoa(safeCount)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	safeCount := 0
+
+	for _, lineStr := range input {
+		var line = []int{}
+		for _, char := range strings.Split(lineStr, " ") {
+			number, err := strconv.Atoi(char)
+			if err != nil {
+				panic(err)
+			}
+			line = append(line, number)
+		}
+
+		isDecreasing := line[0] > line[1]
+
+		if isDecreasing {
+			errorCount := 0
+			for index, number := range line {
+				if index > 0 {
+					diff := number - line[index-1]
+					if diff > 0 {
+						// different directions
+						errorCount++
+					}
+					if diff < -3 || diff == 0 {
+						errorCount++
+						break
+					}
+				}
+			}
+			if errorCount <= 1 {
+				safeCount++
+			}
+		} else {
+			errorCount := 0
+			for index, number := range line {
+				if index > 0 {
+					diff := number - line[index-1]
+					if diff < 0 {
+						// different directions
+						errorCount++
+					}
+					if diff > 3 || diff == 0 {
+						errorCount++
+					}
+				}
+			}
+			if errorCount <= 1 {
+				safeCount++
+			}
+		}
+	}
+
+	return strconv.Itoa(safeCount)
 }
